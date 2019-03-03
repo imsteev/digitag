fea53d6da7ce7d330976b01ba71a1772da1122f8
Stephen Chung <spchung@andrew.cmu.edu>
2019-03-02 20:44:57 -0800

    svg files part 1

--- static/js/laundrySymbols.js
+++ static/js/laundrySymbols.js
@@ -1,67 +1,88 @@
 // washing
 const washing = {
   MACHINE: {'description': 'Machine wash', 'url': './static/assets/machine-wash.svg'},
-  HAND: {'description': 'Hand wash', 'url': './static/assets/machine-wash.svg'},
-  DONT: {'description': 'Do not wash', 'url': './static/assets/machine-wash.svg'},
-  DRY_CLEAN: {'description': 'Dry clean only', 'url': './static/assets/machine-wash.svg'},
-  DONT_DRY_CLEAN: {'description': 'Do not dry clean', 'url': './static/assets/machine-wash.svg'}
+  HAND: {'description': 'Hand wash', 'url': './static/assets/hand-wash.svg'},
+  DONT: {'description': 'Do not wash', 'url': './static/assets/dont-wash.svg'},
+  DRY_CLEAN: {'description': 'Dry clean only', 'url': './static/assets/dry-clean.svg'},
+  DONT_DRY_CLEAN: {'description': 'Do not dry clean', 'url': './static/assets/dont-dry-clean.svg'}
 }
 
 const washingTemps = {
-  COLD: {'description': 'Wash cold', 'min': '65F', 'max': '85F', 'url': './static/assets/machine-wash.svg'},
-  WARM: {'description': 'Wash warm', 'min': '85F', 'max': '105F', 'url': './static/assets/machine-wash.svg'},
-  HOT: {'description': 'Wash hot', 'min': '105F', 'max': '120F', 'url': './static/assets/machine-wash.svg'},
+  COLD: {'description': 'Wash cold', 'min': '65F', 'max': '85F', 'url': './static/assets/wash-temp-1.svg'},
+  WARM: {'description': 'Wash warm', 'min': '85F', 'max': '105F', 'url': './static/assets/wash-temp-2.svg'},
+  HOT: {'description': 'Wash hot', 'min': '105F', 'max': '120F', 'url': './static/assets/wash-temp-3.svg'},
 }
 
 const washingCycles = {
-  NORMAL: {'description': 'Normal cycle', 'url': './static/assets/machine-wash.svg'},
-  PERMANENT: {'description': 'Permanent press cycle', 'url': './static/assets/machine-wash.svg'},
-  DELICATE: {'description': 'Delicate/gentle cycle', 'url': './static/assets/machine-wash.svg'}
+  NORMAL: {'description': 'Normal cycle', 'url': './static/assets/FILL-IN-FILE.svg'},
+  PERMANENT: {'description': 'Permanent press cycle', 'url': './static/assets/FILL-IN-FILE.svg'},
+  DELICATE: {'description': 'Delicate/gentle cycle', 'url': './static/assets/FILL-IN-FILE.svg'}
 }
 
 // drying
 const drying = {
-  TUMBLE: {'description': 'Tumble drying allowed', 'url': './static/assets/machine-wash.svg'},
-  DONT_TUMBLE: {'description': 'Do not tumble dry', 'url': './static/assets/machine-wash.svg'},
-  HANG: {'description': 'Hang to dry', 'url': './static/assets/machine-wash.svg'},
-  FLAT: {'description': 'Dry flat', 'url': './static/assets/machine-wash.svg'},
-  DONT_WRING: {'description': 'Do not wring', 'url': './static/assets/machine-wash.svg'}
+  TUMBLE: {'description': 'Tumble drying allowed', 'url': './static/assets/tumble-dry.svg'},
+  DONT_TUMBLE: {'description': 'Do not tumble dry', 'url': './static/assets/dont-tumble-dry.svg'},
+  HANG: {'description': 'Hang to dry', 'url': './static/assets/line-dry.svg'},
+  FLAT: {'description': 'Dry flat', 'url': './static/assets/dry-flat.svg'},
+  DONT_WRING: {'description': 'Do not wring', 'url': './static/assets/FILL-IN-FILE.svg'}
 }
 
 const dryingTemps = {
-  ANY: {'description': 'Any heat', 'url': './static/assets/machine-wash.svg'},
-  LOW: {'description': 'Low heat', 'url': './static/assets/machine-wash.svg'},
-  MEDIUM: {'description': 'Medium heat', 'url': './static/assets/machine-wash.svg'},
-  HIGH: {'description': 'High heat', 'url': './static/assets/machine-wash.svg'},
-  NO_HEAT: {'description': 'No heat. Air dry', 'url': './static/assets/machine-wash.svg'}
+  ANY: {'description': 'Any heat', 'url': './static/assets/FILL-IN-FILE.svg'},
+  LOW: {'description': 'Low heat', 'url': './static/assets/FILL-IN-FILE.svg'},
+  MEDIUM: {'description': 'Medium heat', 'url': './static/assets/FILL-IN-FILE.svg'},
+  HIGH: {'description': 'High heat', 'url': './static/assets/FILL-IN-FILE.svg'},
+  NO_HEAT: {'description': 'No heat. Air dry', 'url': './static/assets/FILL-IN-FILE.svg'}
 }
 
 const dryingCycles = {
-  NORMAL: {'description': 'Normal cycle', 'url': './static/assets/machine-wash.svg'},
-  PERMANENT: {'description': 'Permanent press cycle', 'url': './static/assets/machine-wash.svg'},
-  DELICATE: {'description': 'Delicate/gentle cycle', 'url': './static/assets/machine-wash.svg'}
+  NORMAL: {'description': 'Normal cycle', 'url': './static/assets/FILL-IN-FILE.svg'},
+  PERMANENT: {'description': 'Permanent press cycle', 'url': './static/assets/FILL-IN-FILE.svg'},
+  DELICATE: {'description': 'Delicate/gentle cycle', 'url': './static/assets/FILL-IN-FILE.svg'}
 }
 
 // other
 const bleaching = {
-  ALLOWED: {'description': 'Bleaching allowed', 'url': './static/assets/machine-wash.svg'},
-  DONT: {'description': 'Do not bleach', 'url': './static/assets/machine-wash.svg'},
-  NON_CHLORINE: {'description': 'Use non-chlorine bleach', 'url': './static/assets/machine-wash.svg'},
+  ALLOWED: {'description': 'Bleaching allowed', 'url': './static/assets/bleaching-allowed.svg'},
+  DONT: {'description': 'Do not bleach', 'url': './static/assets/dont-bleach.svg'},
+  NON_CHLORINE: {'description': 'Use non-chlorine bleach', 'url': './static/assets/non-chlorine-bleach.svg'},
 }
 
 const ironing = {
-  LOW: {'description': 'Iron low', 'url': './static/assets/machine-wash.svg'},
-  MEDIUM: {'description': 'Iron medium', 'url': './static/assets/machine-wash.svg'},
-  HIGH: {'description': 'Iron high', 'url': './static/assets/machine-wash.svg'},
-  DONT: {'description': 'Do not iron', 'url': './static/assets/machine-wash.svg'},
-  NO_STEAM: {'description': 'No steam added to iron', 'url': './static/assets/machine-wash.svg'},
+  LOW: {'description': 'Iron low', 'url': './static/assets/iron-low.svg'},
+  MEDIUM: {'description': 'Iron medium', 'url': './static/assets/iron-medium.svg'},
+  HIGH: {'description': 'Iron high', 'url': './static/assets/iron-high.svg'},
+  DONT: {'description': 'Do not iron', 'url': './static/assets/dont-iron.svg'},
+  NO_STEAM: {'description': 'No steam added to iron', 'url': './static/assets/FILL-IN-FILE.svg'},
 }
 
-let allSymbols = Object.values(washing)
-  .concat(Object.values(washingTemps))
-  .concat(Object.values(washingCycles))
-  .concat(Object.values(drying))
-  .concat(Object.values(dryingTemps))
-  .concat(Object.values(dryingCycles))
-  .concat(Object.values(bleaching))
-  .concat(Object.values(ironing))
+let allSymbols = [
+  washing.MACHINE,
+  washing.HAND,
+  washing.DONT,
+  washing.DRY_CLEAN,
+  washing.DONT_DRY_CLEAN,
+  washingTemps.COLD,
+  washingTemps.WARM,
+  washingTemps.HOT,
+  washingCycles.NORMAL,
+  washingCycles.PERMANENT,
+  washingCycles.DELICATE,
+  drying.TUMBLE,
+  drying.DONT_TUMBLE,
+  drying.HANG,
+  drying.FLAT,
+  drying.DONT_WRING,
+  dryingCycles.NORMAL,
+  dryingCycles.PERMANENT,
+  dryingCycles.DELICATE,
+  bleaching.ALLOWED,
+  bleaching.DONT,
+  bleaching.NON_CHLORINE,
+  ironing.LOW,
+  ironing.MEDIUM,
+  ironing.HIGH,
+  ironing.DONT,
+  ironing.NO_STEAM
+].filter((symbol, i) => !symbol.url.endsWith('FILL-IN-FILE.svg'))
