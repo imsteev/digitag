befa0497b567bdbba0a26ac68af924c4b65d4001
Stephen Chung <spchung@andrew.cmu.edu>
2019-03-04 22:01:09 -0800

    cache name

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,17 +1,21 @@
+const CACHE_NAME = 'cache-v1'
+
+let staticUrls = [
+  '/static/',
+  '/static/html',
+  '/static/html/index.html',
+  '/static/js',
+  '/static/js/app.js',
+  '/static/js/event-bus.js',
+  '/static/js/laundry-symbols.js',
+  '/static/assets',
+  // TODO: all the .svg files. maybe use a function to generate
+]
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('static').then(cache => {
-      return cache.addAll([
-        '/static/',
-        '/static/html',
-        '/static/html/index.html',
-        '/static/js',
-        '/static/js/app.js',
-        '/static/js/event-bus.js',
-        '/static/js/laundry-symbols.js',
-        '/static/assets',
-        // TODO: all the .svg files. maybe use a function to generate
-      ])
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(staticUrls)
     })
   )
 });
