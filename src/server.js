d5c8392463aa5646896d5894ad5ef51e58b927f3
Stephen Chung <spchung95@gmail.com>
2019-03-05 00:18:53 -0800

    update package.json

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const https = require('https')
 const path = require('path')
 
 const PORT = process.env.PORT || 3000
@@ -11,7 +13,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+const serverOptions = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+}
 // NOTE: HTTPS is required in order to serve a ServiceWorker
-app.listen(PORT, () => {
+https.createServer(serverOptions, app).listen(PORT, () => {
   console.log('server running at port: ' + PORT)
 })
